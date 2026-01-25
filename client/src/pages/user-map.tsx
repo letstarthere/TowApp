864dc4113aeb0fbe18aa81a103cfba9fa4ae9321
Sean Bampoe <bampoesean@gmail.com>
2026-01-25 10:38:57 +0200

    UI fixes

--- client/src/pages/user-map.tsx
+++ client/src/pages/user-map.tsx
@@ -856,6 +856,14 @@ export default function UserMap() {
     );
   }
 
+  if (!location) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-orange-500"></div>
+      </div>
+    );
+  }
+
   return (
     <>
       {showWelcomePopup && (
